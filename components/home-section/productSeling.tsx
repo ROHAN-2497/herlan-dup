99a8062ba3006bb778dcb7f3b64a566262ca28a5
Rohan_Islam <rohanislam3669@gmail.com>
2024-06-25 18:34:51 +0600

    update

--- components/home-section/productSeling.tsx
+++ components/home-section/productSeling.tsx
@@ -1,10 +1,88 @@
+"use client";
 import React from "react";
-
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 const ProductSeling = () => {
   return (
     <div>
       <div>Selling Fast</div>
-      <div> </div>
+      <div>
+        {" "}
+        <Swiper
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img
+              width={400}
+              height={300}
+              src="https://swiperjs.com/demos/images/nature-1.jpg"
+              loading="lazy"
+            />
+            <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://swiperjs.com/demos/images/nature-2.jpg"
+              loading="lazy"
+            />
+            <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://swiperjs.com/demos/images/nature-3.jpg"
+              loading="lazy"
+            />
+            <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://swiperjs.com/demos/images/nature-4.jpg"
+              loading="lazy"
+            />
+            <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://swiperjs.com/demos/images/nature-5.jpg"
+              loading="lazy"
+            />
+            <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://swiperjs.com/demos/images/nature-6.jpg"
+              loading="lazy"
+            />
+            <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://swiperjs.com/demos/images/nature-7.jpg"
+              loading="lazy"
+            />
+            <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://swiperjs.com/demos/images/nature-8.jpg"
+              loading="lazy"
+            />
+            <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://swiperjs.com/demos/images/nature-9.jpg"
+              loading="lazy"
+            />
+            <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+          </SwiperSlide>
+        </Swiper>{" "}
+      </div>
     </div>
   );
 };
