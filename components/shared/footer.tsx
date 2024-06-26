54ef60359cb3fb44a0bd15a6eaa830de36a59ef0
Rohan_Islam <rohanislam3669@gmail.com>
2024-06-27 00:25:05 +0600

    update

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -6,8 +6,8 @@ const Footer = () => {
   return (
     <footer>
       <div className="container">
-        <div>
-          <div>
+        <div className="flex mt-20">
+          <div className="">
             <img src="/Herlan-Store-Website-Logo.png" alt="" />
             <div className="flex items-center">
               {" "}
@@ -16,14 +16,30 @@ const Footer = () => {
               <Instagram />
             </div>
           </div>
-          <div>
-            <h3>About</h3> <p>About Us</p> <p>Terms & Conditions</p>{" "}
-            <p>Privacy Policy</p> <p>Return and Refund Policy</p> <p>Career</p>
+          <div className="w-1/2">
+            <h3 className="font-bold mb-2">About</h3>{" "}
+            <p className="py-2">About Us</p> <p>Terms & Conditions</p>{" "}
+            <p className="py-2">Privacy Policy</p>{" "}
+            <p>Return and Refund Policy</p> <p className="py-2">Career</p>
           </div>
-          <div>
-            <h3>Help</h3> <p>Store Locator</p> <p>Contact Us</p>
+          <div className="w-1/2">
+            <h3 className="font-bold mb-2">Help</h3>{" "}
+            <p className="py-2">Store Locator</p> <p>Contact Us</p>
           </div>
-          <div></div>
+          <div className="w-full">
+            <p>Join our newsletter to access all the benefits.</p>
+            <p>Sign up to receive the latest news & exclusive offers!</p>
+            <div className="mt-10">
+              <input
+                className="border-2"
+                type="text"
+                placeholder="Email address"
+              />
+              <button className="bg-black hover:bg-red-500 text-white font-semibold px-2 py-1 rounded">
+                Sign Up
+              </button>
+            </div>
+          </div>
         </div>
         <div>2</div>
         <div>3</div>
